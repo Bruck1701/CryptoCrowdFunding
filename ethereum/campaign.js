30f3e66ac5a0fef64501b608c1b5e7e0f6ea0136
Bruck1701 <kummel@gmail.com>
2020-05-03 19:14:30 +0200

    access to instance of deployed contract.

---
+++ ethereum/campaign.js
@@ -0,0 +1,20 @@
+import { ethers } from "ethers";
+import Campaign from "./build/Campaign.json";
+
+export default (address) => {
+  const abi = Campaign.abi;
+  let provider;
+
+  console.log(address);
+
+  if (typeof window !== "undefined" && window.web3 !== "undefined") {
+    //running on the browser not on the server and user has metamask installed
+
+    provider = new ethers.providers.Web3Provider(web3.currentProvider);
+  } else {
+    //
+    provider = ethers.getDefaultProvider("rinkeby");
+  }
+
+  return new ethers.Contract(address, abi, provider);
+};
