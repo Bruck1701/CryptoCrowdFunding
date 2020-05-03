cdf288b0e2d8e548f8ed72daff7f4be6b6d31d46
Bruck1701 <kummel@gmail.com>
2020-05-03 19:15:28 +0200

    get information from the contract through the instance in campaign.js

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import Layout from "../../components/layout";
+import Campaign from "../../ethereum/campaign";
+import { Card } from "semantic-ui-react";
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const campaign = Campaign(props.query.address);
+    const summary = await campaign.functions.getSummary();
+    //console.log(summary[0].toString());
+
+    return {
+      contractName: props.query.address,
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      backersCount: summary[3],
+      manager: summary[4],
+    };
+  }
+
   render() {
     return (
       <Layout>
-        <h3> Campaign Details for </h3>
+        <h3> Campaign Details for {this.contractName}</h3>
       </Layout>
     );
   }
