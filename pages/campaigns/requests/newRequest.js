327989563d79f25e170fb7268c8aaf01d07d3c2f
Bruck1701 <kummel@gmail.com>
2020-05-05 14:42:02 +0200

    Create new request page

---
+++ pages/campaigns/requests/newRequest.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Layout from "../../../components/layout";
+import { Grid, Form, Button } from "semantic-ui-react";
+
+class RequestNew extends Component {
+  render() {
+    return (
+      <Layout>
+        <h3> Create a Request </h3>
+      </Layout>
+    );
+  }
+}
+
+export default RequestNew;
